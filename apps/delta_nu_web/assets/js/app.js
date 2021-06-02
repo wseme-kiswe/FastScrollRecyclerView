6cd989808efcc884c267d182908d694102c64f91
wseme-kiswe <walter.seme@kiswe.com>
2021-06-02 09:34:13 -0400

    added alpinejs for modal button

--- apps/delta_nu_web/assets/js/app.js
+++ apps/delta_nu_web/assets/js/app.js
@@ -1,8 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
-
+import 'alpinejs'
+import '../css/app.scss'
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
@@ -12,4 +12,4 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
+import 'phoenix_html'
